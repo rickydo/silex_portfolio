2bf826ce90adc05e87fd99d8e2972174823bbf31
rickydo <g3tdo@yahoo.com>
2015-02-03 10:48:15 -0600

    registered URL generator, twig service, index.twig

--- web/app.php
+++ web/app.php
@@ -2,10 +2,23 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use Silex\Application;
+
+class PortfolioApplication extends Application {
+	use Application\TwigTrait;
+}
+
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__.'/views',
+));
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app = new Application();
 $app['debug'] = true;
 
 $app->get('/', function(){
-	return "Hello!";
+	return $app->rener('index.php.twig');
 });
 $app->run();
