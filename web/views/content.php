1ea610e583e156e6dd86e3437c201b801ad20d95
rickydo <g3tdo@yahoo.com>
2015-02-08 15:35:58 -0600

    have first two sections of clider working

---
+++ web/views/content.php
@@ -0,0 +1,54 @@
+<div class="container">
+		<div class="wheel">
+		</div>
+		<div class="top_row">
+			<div class="column" id="intro">
+				<ul>
+					<li>Biologist turned web developer</li>
+					<li>Mechanically inclined</li>
+					<li>Building things with purpose</li>
+					<li>Netflixing</li>
+				</ul>
+			</div>
+			<div class="column" id="skills">
+				<ul>
+					<li>Rails</li>
+					<li>JS 
+						<ul>
+							<li>AJAX</li>
+							<li>JQuery</li>
+						</ul>
+					</li>
+					<li>HTML, CSS, SASS</li>
+					<li>PHP 
+						<ul>
+							<li>Laravel, Composer, MySQL</li>
+							<li>Blade, Foundation</li>
+							<li>Eloquent, Twig</li>
+						</ul>
+					</li>
+					<li>XAMP, MAMP</li>
+				</ul>
+			</div>
+		</div>
+		<div class="middle_row">
+			<div class="wheel_hub column">
+				<div class="icon_row">
+					<a href="https://twitter.com/statusd0" target="_blank"><i class="fi-social-twitter"></i></a>
+					<a href="https://www.facebook.com/phucee" target="_blank"><i class="fi-social-facebook"></i></a>
+				</div>
+				<div class="icon_row">
+					<a href="https://github.com/rickydo" target="_blank"><i class="fi-social-github"></i></a>
+					<a href="mailto:ricky.dop@gmail.com" target="_blank"><i class="fi-mail"></i></a>
+				</div>
+			</div>
+		</div>
+		<div class="bottom_row">
+			<div class="column" id="cover_letter">
+				Cover Letter Preview
+			</div>
+			<div class="column" id="projects">
+				Projects Link, maybe a collage of projects
+			</div>
+		</div>		
+	</div>
